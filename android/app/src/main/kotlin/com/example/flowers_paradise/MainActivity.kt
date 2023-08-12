16bbb48b442af91ac8cb7bc8259c016beb23dcdf
Yelsin Sánchez <syelsinalbert@gmail.com>
2023-08-12 10:34:17 -0600

    proyecto_final

---
+++ android/app/src/main/kotlin/com/example/flowers_paradise/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flowers_paradise
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/k_strings.dart
@@ -0,0 +1,16 @@
+class KStrings {
+  static const String appName = "Flowers Paradise";
+  static const String appVersion = "1.0.0";
+  static const String splashTitle = "La floristería más hermosa del mundo";
+
+  static const String cachedUserResponseKey = "cachhUserResponse";
+  static const String cachOnboardingKey = "cachOnboardingKey";
+  static const String cachedWebSettingKey = "cachedWebSettingKey";
+  static const String cacheCouponResponse = "couponResponseKey";
+  static const String cacheCartCalculation = "cartCalculationKey";
+  static const String bankInfoHint = ""
+      "Name: GRUPO 5\n\n"
+      "Bank: LALALA\n\n"
+      "A/C: 098**********7231\n\n"
+      "Branch: JUANITO PEPE\n";
+}
